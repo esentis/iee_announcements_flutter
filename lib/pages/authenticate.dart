7e6a0c02ecc39dcf04a221cfc74a8142a08f4b81
George Leonidis <esentakos@yahoo.gr>
2021-01-29 20:28:11 +0200

    Refactors code

--- lib/pages/authenticate.dart
+++ lib/pages/authenticate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:iee_announcements_flutter/connection/iee_api_service.dart';
+import 'package:iee_announcements_flutter/constants.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:logger/logger.dart';
 
@@ -8,14 +11,23 @@ class AuthenticationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Flexible(
-        child: WebView(
-          initialUrl:
-              'https://login.it.teithe.gr/authorization/?client_id=600ca98874f76d554d48592d&response_type=code&scope=announcements&redirect_uri=https://www.github.com/esentis',
-          onPageFinished: (url) {
-            logger.wtf(url);
-          },
-        ),
+      child: WebView(
+        javascriptMode: JavascriptMode.unrestricted,
+        initialUrl:
+            '$kLoginUrl?client_id=${env['CLIENT_ID']}&response_type=code&scope=announcements,refresh_token,profile&redirect_uri=${env['RESPONSE_URL']}',
+        onPageFinished: (url) {
+          logger.wtf(url);
+
+          // Code is at specific position in response url
+          var code = url.substring(32, 57);
+          logger.wtf(code);
+          if (!isNumeric(url.substring(32, 57))) {
+            logger.wtf('Not signed in yet');
+          } else {
+            getAccessToken(url.substring(32, 57));
+            Navigator.pop(context);
+          }
+        },
       ),
     );
   }
