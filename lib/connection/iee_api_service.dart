fddfe194dd19b5c8f14e542ababa2d37b576821f
George Leonidis <esentakos@yahoo.gr>
2021-02-01 00:50:44 +0200

    Adds refresh tokens on SharedPreferences

--- lib/connection/iee_api_service.dart
+++ lib/connection/iee_api_service.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:iee_announcements_flutter/constants.dart';
+import 'package:iee_announcements_flutter/main.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 var logger = Logger();
 BaseOptions apiOptions = BaseOptions(
@@ -12,7 +14,7 @@ BaseOptions apiOptions = BaseOptions(
 );
 Dio http = Dio(apiOptions);
 
-/// Gets an Access Token
+/// Gets an Access Token with the authorization code returned after authorizing app's access.
 Future getAccessToken(String code) async {
   Response response;
   try {
@@ -24,13 +26,11 @@ Future getAccessToken(String code) async {
         'grant_type': 'authorization_code',
         'code': code
       },
-      options: Options(
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      ),
     );
     logger.i('Getting access token');
+    var _prefs = await SharedPreferences.getInstance();
+    // Saves the refresh token in SharedPreferences
+    await _prefs.setString('refresh_token', response.data['refresh_token']);
     logger.wtf(response.data);
     return response.data;
   } on DioError catch (e) {
@@ -40,8 +40,9 @@ Future getAccessToken(String code) async {
 }
 
 /// Refreshes a token
-Future refreshToken(String token) async {
+Future getRefreshToken() async {
   Response response;
+  var _prefs = await SharedPreferences.getInstance();
   try {
     response = await http.post(
       kTokenUrl,
@@ -49,15 +50,14 @@ Future refreshToken(String token) async {
         'client_id': env['CLIENT_ID'],
         'client_secret': env['SECRET'],
         'grant_type': 'refresh_token',
-        'code': token,
+        'code': _prefs.getString('refresh_token'),
       },
-      options: Options(
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      ),
     );
     logger.i('Getting access token');
+    // Saves the new refresh token in SharedPreferences
+    await _prefs.setString('refresh_token', response.data['refresh_token']);
+    userSession.accessToken = response.data['access_token'];
+    userSession.refreshToken = response.data['refresh_token'];
     logger.wtf(response.data);
     return response.data;
   } on DioError catch (e) {
@@ -71,6 +71,25 @@ Future getProfile(String accessToken) async {
   Response response;
   try {
     response = await http.get('${kApiUrl}profile',
+        options: Options(headers: {
+          'x-access-token': accessToken,
+          'Content-Type': 'application/json'
+        }));
+    logger.i('Getting profile info');
+    logger.wtf(response.data);
+    return response.data;
+  } on DioError catch (e) {
+    logger.e(e);
+    return e.type;
+  }
+}
+
+/// Gets profile info
+Future getAnnouncenemts(String accessToken) async {
+  Response response;
+  logger.wtf('GET ${kApiUrl}announcements');
+  try {
+    response = await http.get('${kApiUrl}announcements',
         options: Options(headers: {
           'x-access-token': accessToken,
           'Content-Type': 'application/json'
